2c1bca38a72d1117f36cbe67ff11b853d4a64af6
martin.soisch <martin.soisch@SCF41.ga.supplychainfactory.com>
2018-05-14 11:07:04 +0200

    Remove: IN ALL

--- src/Kendo/DataSource.php
+++ src/Kendo/DataSource.php
@@ -25,7 +25,6 @@ class DataSource {
     );
     protected $arrayOperators = array(
         'eq' => 'IN',
-        'eq_all' => 'IN ALL',
         'neq' => 'NOT IN'
     );
     protected $dbArrayOperators = array(
@@ -376,8 +375,6 @@ class DataSource {
                 return "$alias $operator";
             } else if ($operator === 'IN') {
                 return "$alias = ANY($value)";
-            } else if ($operator === 'IN ALL') {
-                return "$alias = ALL($value)";
             } else if ($operator === 'NOT IN') {
                 return "NOT $alias = ANY($value)";
             } else if ($operator === 'CUSTOM') {
